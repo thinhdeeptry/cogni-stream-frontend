8a25bcf099c6096d02bb8e716c64bd92a82353cb
thanhcanhit <thanhcanh.dev@gmail.com>
2025-03-01 15:27:24 +0700

    Config react-bits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import { Metadata } from "next";
 
+import SplitText from "@/components/react-bits/SplitText/SplitText";
+
 export const metadata: Metadata = {
   title: "EduForge - Your Next Education Platform",
   description:
@@ -12,14 +14,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <SplitText
+          text="Edu Forge"
+          className="text-5xl leading-normal font-bold"
+        ></SplitText>
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -45,7 +44,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Gay now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
